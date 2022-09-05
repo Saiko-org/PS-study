02daafade854be0d97e9ed6a3c9d456147eaba24
UJ15 <dbwogml15@naver.com>
2022-09-05 12:46:05 +0900

    fail : 모의테스트7 4

---
+++ mockTest7/4-UJ.java
@@ -0,0 +1,11 @@
+/**
+ * 프로그래머스 : 옮기기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/77886
+ * 소요시간 : X
+ */
+class Solution {
+  public String[] solution(String[] s) {
+    String[] answer = {};
+    return answer;
+  }
+}
