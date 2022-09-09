79d3f9558e6fa0ea7092ccfea6ded386434d53b9
wogha95 <wogha95@naver.com>
2022-09-10 08:38:04 +0900

    add: 모의테스트9 3

--- mockTest9/3-jaeho.js
+++ mockTest9/3-jaeho.js
@@ -1,6 +1,7 @@
 // 프로그래머스 / 72412 / 순위검색
 // https://school.programmers.co.kr/learn/courses/30/lessons/72412
 // fail: 1시간
+// add: 황수경님의 도움으로 해결
 
 const LANGUAGENAME = ["-", "cpp", "java", "python"];
 const POSITIONNAME = ["-", "backend", "frontend"];
@@ -9,21 +10,14 @@ const FOODNAME = ["-", "chicken", "pizza"];
 
 function solution(info, query) {
   const answer = [];
-  const persons = [];
-
   const map = new Map();
 
   // 모든 경우의 수에 빈배열 만들기 (안그러면 런타임 아웃 발생)
-  for (let l = 0; l < 4; l++) {
-    for (let p = 0; p < 3; p++) {
-      for (let c = 0; c < 3; c++) {
-        for (let f = 0; f < 3; f++) {
-          const key = [
-            LANGUAGENAME[l],
-            POSITIONNAME[p],
-            CAREERNAME[c],
-            FOODNAME[f],
-          ].join("");
+  for (const keyL of LANGUAGENAME) {
+    for (const keyP of POSITIONNAME) {
+      for (const keyC of CAREERNAME) {
+        for (const keyF of FOODNAME) {
+          const key = [keyL, keyP, keyC, keyF].join("");
           map.set(key, []);
         }
       }
@@ -34,17 +28,12 @@ function solution(info, query) {
   for (const i of info) {
     const [language, position, career, food, score] = i.split(" ");
 
-    // 깨름칙하지만 나중에 접근할때 빠르도록 모든 경우의 수에 대응하도록 만들기
-    for (let l = 0; l < 2; l++) {
-      const keyL = l === 0 ? "-" : language;
-      for (let p = 0; p < 2; p++) {
-        const keyP = p === 0 ? "-" : position;
-        for (let c = 0; c < 2; c++) {
-          const keyC = c === 0 ? "-" : career;
-          for (let f = 0; f < 2; f++) {
-            const keyF = f === 0 ? "-" : food;
+    for (const keyL of ["-", language]) {
+      for (const keyP of ["-", position]) {
+        for (const keyC of ["-", career]) {
+          for (const keyF of ["-", food]) {
             const key = [keyL, keyP, keyC, keyF].join("");
-            map.set(key, [...map.get(key), parseInt(score)]);
+            map.get(key).push(parseInt(score));
           }
         }
       }
@@ -53,10 +42,7 @@ function solution(info, query) {
 
   // 이진 탐색을 위해 정렬
   for (const [key, value] of map) {
-    map.set(
-      key,
-      value.sort((a, b) => a - b)
-    );
+    value.sort((a, b) => a - b);
   }
 
   // 각 조건에 맞는 인원수 구하기
@@ -87,6 +73,7 @@ function solution(info, query) {
         }
       }
 
+      // 동일 점수가 되는 곳의 제일 작은 index 찾기
       while (0 <= mid && score <= values[mid]) {
         mid -= 1;
       }
