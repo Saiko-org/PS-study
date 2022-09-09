ff41a897130852cedef0c5248adaedebb9e14deb
Hoseok Na <hsna7024@gmail.com>
2022-09-09 22:18:00 +0900

    add: 프로그래머스 118670

--- freePS/118670-hoseokna.js
+++ freePS/118670-hoseokna.js
@@ -2,6 +2,84 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/118670
 // fail: 1시간
 
+function Node(data) {
+  this.data = data
+  this.prev = null
+  this.next = null
+}
+
+function Queue() {
+  this.head = null
+  this.tail = null
+  this.size = 0
+
+  this.insertHead = (data) => {
+    const node = new Node(data)
+
+    this.size++
+
+    if (this.head === null) {
+      this.head = node
+      this.tail = node
+
+      return
+    }
+
+    node.next = this.head
+    this.head.prev = node
+    this.head = node
+  }
+
+  this.insertTail = (data) => {
+    const node = new Node(data)
+
+    this.size++
+
+    if (this.tail === null) {
+      this.head = node
+      this.tail = node
+
+      return
+    }
+
+    node.prev = this.tail
+    this.tail.next = node
+    this.tail = node
+  }
+
+  this.popHead = () => {
+    const popped = this.head.data
+
+    if (this.head === this.tail) {
+      this.tail = null
+    }
+
+    this.head = this.head.next
+    if (this.head) {
+      this.head.prev = null
+    }
+    this.size--
+
+    return popped
+  }
+
+  this.popTail = () => {
+    const popped = this.tail.data
+
+    if (this.tail === this.head) {
+      this.head = null
+    }
+
+    this.tail = this.tail.prev
+    if (this.tail) {
+      this.tail.next = null
+    }
+    this.size--
+
+    return popped
+  }
+}
+
 function solution(rc, operations) {
   const COMMAND = Object.freeze({
     SHIFT_ROW: 'ShiftRow',
@@ -10,53 +88,63 @@ function solution(rc, operations) {
   const Y_LENGTH = rc.length
   const X_LENGTH = rc[0].length
 
-  const getShiftRow = (rc) => {
-    const nextRC = Array.from(new Array(Y_LENGTH), () =>
-      new Array(X_LENGTH).fill(0)
-    )
+  const rows = new Queue()
+  const firstColumns = new Queue()
+  const lastColumns = new Queue()
 
-    for (let y = 0; y < Y_LENGTH; y++) {
-      for (let x = 0; x < X_LENGTH; x++) {
-        y !== Y_LENGTH - 1
-          ? (nextRC[y + 1][x] = rc[y][x])
-          : (nextRC[0][x] = rc[y][x])
-      }
-    }
+  rc.forEach((columns) => {
+    const queue = new Queue()
 
-    return nextRC
+    columns.slice(1, -1).forEach((column) => queue.insertTail(column))
+    rows.insertTail(queue)
+  })
+
+  for (let y = 0; y < Y_LENGTH; y++) {
+    firstColumns.insertTail(rc[y][0])
+    lastColumns.insertTail(rc[y][X_LENGTH - 1])
   }
 
-  const getRotated = (rc) => {
-    const nextRC = rc.map((columns) => [...columns])
+  const shiftRow = () => {
+    rows.insertHead(rows.popTail())
+    firstColumns.insertHead(firstColumns.popTail())
+    lastColumns.insertHead(lastColumns.popTail())
+  }
 
-    for (let x = 0; x < X_LENGTH - 1; x++) {
-      nextRC[0][x + 1] = rc[0][x]
-    }
-
-    for (let y = 0; y < Y_LENGTH - 1; y++) {
-      nextRC[y + 1][X_LENGTH - 1] = rc[y][X_LENGTH - 1]
-    }
-
-    for (let x = X_LENGTH - 1; x > 0; x--) {
-      nextRC[Y_LENGTH - 1][x - 1] = rc[Y_LENGTH - 1][x]
-    }
-
-    for (let y = Y_LENGTH - 1; y > 0; y--) {
-      nextRC[y - 1][0] = rc[y][0]
-    }
-
-    return nextRC
+  const rotate = () => {
+    rows.tail.data.insertTail(lastColumns.popTail())
+    firstColumns.insertTail(rows.tail.data.popHead())
+    rows.head.data.insertHead(firstColumns.popHead())
+    lastColumns.insertHead(rows.head.data.popTail())
   }
 
   operations.forEach((operation) => {
     if (operation === COMMAND.SHIFT_ROW) {
-      rc = getShiftRow(rc)
+      shiftRow()
 
       return
     }
 
-    rc = getRotated(rc)
+    rotate()
   })
 
-  return rc
+  const answer = []
+
+  for (let y = 0; y < Y_LENGTH; y++) {
+    const row = []
+
+    row.push(firstColumns.popHead())
+
+    const rowsQueue = rows.popHead()
+    let head = rowsQueue.head
+
+    while (head) {
+      row.push(head.data)
+      head = head.next
+    }
+
+    row.push(lastColumns.popHead())
+    answer.push(row)
+  }
+
+  return answer
 }
