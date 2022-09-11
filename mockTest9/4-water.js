7c6ea45723ce3887536f259f90ee063c111e44fb
waterglasses <hsk10271@gmail.com>
2022-09-11 22:08:35 +0900

    fail: 모의테스트 9 4

---
+++ mockTest9/4-water.js
@@ -0,0 +1,8 @@
+// 프로그래머스 / 72414 / 광고 삽입
+// https://school.programmers.co.kr/learn/courses/30/lessons/72414
+// fail: 1시간
+
+function solution(play_time, adv_time, logs) {
+  var answer = '';
+  return answer;
+}
