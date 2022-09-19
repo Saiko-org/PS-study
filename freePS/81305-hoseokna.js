05ca13534c576b799ebf1b4da6e788d7c52c3176
Hoseok Na <hsna7024@gmail.com>
2022-09-19 23:34:04 +0900

    add: 프로그래머스 81305

--- freePS/81305-hoseokna.js
+++ freePS/81305-hoseokna.js
@@ -1,60 +1,76 @@
 /**
  * 프로그래머스 / 81305 / 시험장 나누기
  * https://school.programmers.co.kr/learn/courses/30/lessons/81305
- * fail: 1시간
+ * add: https://ltk3934.tistory.com/184
+ *
+ * js에서는 효율성에서 런타임 에러(스택 초과).
  */
 
-const getCombination = (array, number) => {
-  const result = []
-
-  if (number === 1) {
-    return array.map((v) => [v])
-  }
-
-  array.forEach((fixed, index) => {
-    const rest = array.slice(fixed)
-    const restCombination = getCombination(rest, number - 1)
-    const combined = restCombination.map((v) => [fixed, ...v])
-
-    result.push(...combined)
-  })
-
-  return result
+function Tree(id) {
+  this.id = id
+  this.parent = -1
+  this.left = -1
+  this.right = -1
 }
 
 function solution(k, num, links) {
   const N = num.length
-
-  if (k === 1) {
-    return num.reduce((acc, cur) => (acc += cur))
-  }
-
-  if (k === N) {
-    return Math.max(...num)
-  }
-
-  const graph = Array.from(new Array(N), () => [])
-  const lines = []
+  const trees = Array.from(new Array(N), (_, index) => new Tree(index))
 
   links.forEach(([left, right], index) => {
+    trees[index].left = left
+    trees[index].right = right
+
     if (left !== -1) {
-      graph[index].push(left)
-      graph[left].push(index)
-      lines.push([index, left])
+      trees[left].parent = index
     }
 
     if (right !== -1) {
-      graph[index].push(right)
-      graph[right].push(index)
-      lines.push([index, right])
+      trees[right].parent = index
     }
   })
 
-  const indexList = Array.from(new Array(N), (_, index) => index + 1)
-  console.log(indexList)
+  const rootId = trees.findIndex(({ parent }) => parent === -1)
 
-  let min = Infinity
-  const combinations = getCombination(indexList, k - 1)
+  let groupCount = 1
 
-  console.log(combinations)
+  const dfs = (id, max) => {
+    const left = trees[id].left
+    const leftSum = left !== -1 ? dfs(left, max) : 0
+    const right = trees[id].right
+    const rightSum = right !== -1 ? dfs(right, max) : 0
+
+    if (num[id] + leftSum + rightSum <= max) {
+      return num[id] + leftSum + rightSum
+    }
+
+    if (num[id] + Math.min(leftSum, rightSum) <= max) {
+      groupCount += 1
+
+      return num[id] + Math.min(leftSum, rightSum)
+    }
+
+    groupCount += 2
+
+    return num[id]
+  }
+
+  const MAX = Math.max(...num)
+  let left = MAX
+  let right = MAX * N
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2)
+
+    groupCount = 1
+    dfs(rootId, mid)
+
+    if (groupCount <= k) {
+      right = mid
+    } else {
+      left = mid + 1
+    }
+  }
+
+  return left
 }
