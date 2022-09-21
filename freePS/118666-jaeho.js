81bcfca0a9150cbadceb2981c45b580faaafdab5
wogha95 <wogha95@naver.com>
2022-09-21 11:33:10 +0900

    solve: 프로그래머스 92334

--- freePS/118666-jaeho.js
+++ freePS/118666-jaeho.js
@@ -1,7 +1,7 @@
-// 프로그래머스 / 118666 / 성격 유형 검사하기
+// 프로그래머스 / 118666 / 성격 유형 검사하기 (Lv1 22 카카오 인턴십)
 // https://school.programmers.co.kr/learn/courses/30/lessons/118666
 // solve: 15분
-// add: 다른 사람의 풀이로 대체 (훨씬 간결하고 함수형으로 작성되어있어서)
+// add: 다른 사람의 풀이로 대체 (훨씬 간결하고 함수형으로 작성되어있어서, 기존에는 절차형으로 풀이했음)
 
 function solution(survey, choices) {
   const MBTI = {};
